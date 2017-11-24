1981cac354468e6b61c552fd98b3df5865b7b276
Ильчуков Александр Сергеевич <a.s.ilchukov@cit.rkomi.ru>
2017-11-24 09:29:16 +0300

    #3 Добавлены миграции, создающие таблицы
    Добавлены миграции, создающие таблицы `requests` и `request_attributes`

---
+++ db/migrations/003_create_requests.rb
@@ -0,0 +1,21 @@
+# encoding: utf-8
+
+# @author Александр Ильчуков <a.s.ilchukov@cit.rkomi.ru>
+#
+# Создание таблицы записей межведомственных запросов, связанных с заявкой
+#
+
+Sequel.migration do
+  change do
+    create_table(:requests) do
+      primary_key :id
+
+      column :created_at, :timestamp, index: true, null: false
+
+      foreign_key :case_id, :cases,
+                  type:      :text,
+                  on_update: :cascade,
+                  on_delete: :cascade
+    end
+  end
+end
