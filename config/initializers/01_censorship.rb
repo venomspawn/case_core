9d443cbe543b39f682a3ddb4a115bee96557cae9
Ильчуков Александр Сергеевич <a.s.ilchukov@cit.rkomi.ru>
2017-12-07 17:10:12 +0300

    #9 Добавлена инфраструктура для фильтрации
    * Добавлен класс `CaseCore::Censorship::Filter`, предоставляющий функцию
      `process`, которая фильтрует ключи ассоциативных массивов и строки,
      слишком длинные для отображения
    
    * Добавлены тесты функции `process` класса
      `CaseCore::Censorship::Filter`
    
    * Добавлена поддержка инфраструктуры фильтрации при инициализации
      приложения

---
+++ config/initializers/01_censorship.rb
@@ -0,0 +1,15 @@
+# encoding: utf-8
+
+# @author Александр Ильчуков <a.s.ilchukov@cit.rkomi.ru>
+#
+# Файл настройки фильтрации ключей ассоциативных массивов при выводе их
+# значений
+#
+
+require "#{$lib}/censorship/filter"
+
+CaseCore::Censorship::Filter.configure do |settings|
+  settings.set :censored_message,    '[CENSORED]'
+  settings.set :too_long_message,    '[TOO LONG]'
+  settings.set :string_length_limit, 40
+end
