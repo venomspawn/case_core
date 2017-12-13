1ce6e689b78f39b148a581a4ab36de828795f755
Ильчуков Александр Сергеевич <a.s.ilchukov@cit.rkomi.ru>
2017-12-13 16:27:44 +0300

    #14 Добавлена поддержка извлечения бизнес-логики по типу заявки
    * Добавлен модуль `CaseCore::Actions::Cases::Mixins::Logic`,
      предоставляющий метод `logic`, который извлекает модуль бизнес-логики
      по значению поля `type` записи заявки
    
    * Добавлены тесты модуля `CaseCore::Actions::Cases::Mixins::Logic`

---
+++ lib/case_core/actions/cases/mixins/logic.rb
@@ -0,0 +1,56 @@
+# encoding: utf-8
+
+require "#{$lib}/helpers/log"
+
+module CaseCore
+  module Actions
+    module Cases
+      # @author Александр Ильчуков <a.s.ilchukov@cit.rkomi.ru>
+      #
+      # Пространство имён для подключаемых модулей
+      #
+      module Mixins
+        # @author Александр Ильчуков <a.s.ilchukov@cit.rkomi.ru>
+        #
+        # Модуль, предоставляющий метод `logic` для извлечения модуля
+        # бизнес-логики по значению поля `type` записи заявки
+        #
+        module Logic
+          include Helpers::Log
+
+          private
+
+          # Сообщение о том, что аргумент не является записью модели
+          # `CaseCore::Models::Case`
+          #
+          NOT_A_CASE =
+            'Аргумент не является записью модели `CaseCore::Models::Case`'
+
+          # Возвращает модуль бизнес-логики для записи заявки по значению поля
+          # `type` или `nil`, если модуль бизнес логики невозможно найти
+          #
+          # @param [CaseCore::Models::Case] c4s3
+          #   запись заявки
+          #
+          # @return [Module]
+          #   модуль бизнес-логики
+          #
+          # @return [NilClass]
+          #   если модуль бизнес-логики невозможно найти
+          #
+          # @raise [ArgumentError]
+          #   если аргумент не является объектом типа {CaseCore::Models::Case}
+          #
+          def logic(c4s3)
+            raise ArgumentError, NOT_A_CASE unless c4s3.is_a?(Models::Case)
+            result = CaseCore::Logic::Loader.logic(c4s3.type)
+            result.tap { log_warn(binding) { <<-LOG } if result.nil? }
+              Невозможно найти модуль бизнес-логики для заявки типа
+              `#{c4s3.type}` с идентификатором записи `#{c4s3.id}`
+            LOG
+          end
+        end
+      end
+    end
+  end
+end
