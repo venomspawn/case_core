8ce333867a85876c649e9d84a6f16247495e6cab
Ильчуков Александр Сергеевич <a.s.ilchukov@cit.rkomi.ru>
2017-12-20 09:40:30 +0300

    #27 Добавлены тесты вызова действия экспорта реестра через STOMP

--- spec/case_core/api/stomp/registers/export_spec.rb
+++ spec/case_core/api/stomp/registers/export_spec.rb
@@ -2,13 +2,14 @@
 
 # @author Александр Ильчуков <a.s.ilchukov@cit.rkomi.ru>
 #
-# Файл тестирования вызова метода модуля бизнес-логики с записью заявки в
-# качестве аргумента с помощью обработчика сообщений STOMP в контроллере класса
+# Файл тестирования вызова функции `export_register` модуля бизнес-логики с
+# записью реестра передаваемой корреспонденции в качестве аргумента с помощью i
+# обработчика сообщений STOMP в контроллере класса
 # `CaseCore::API::STOMP::Controller`
 #
 
 RSpec.describe CaseCore::API::STOMP::Controller do
-  describe 'call of logic function with a case record as an argument' do
+  describe 'call of `export_register` function of logic' do
     include CaseCore::API::STOMP::Controller::Processors::IncomingSpecHelper
 
     before do
@@ -21,7 +22,7 @@ RSpec.describe CaseCore::API::STOMP::Controller do
       allow(CaseCore::API::STOMP::Controller.instance).to receive(:sleep)
 
       CaseCore::Logic::Loader.settings.dir = dir
-      allow(logic).to receive(method_name) unless logic.nil?
+      allow(logic).to receive(:export_register) unless logic.nil?
     end
 
     subject(:run!) { described_class.run! }
@@ -29,24 +30,25 @@ RSpec.describe CaseCore::API::STOMP::Controller do
     let(:message) { create(:stomp_message, headers: headers, body: body) }
     let(:headers) { create_incoming_headers(message_id, entities, action) }
     let(:message_id) { 'id' }
-    let(:entities) { 'cases' }
-    let(:action) { 'call' }
+    let(:entities) { 'registers' }
+    let(:action) { 'export' }
     let(:body) { call_attrs.to_json }
-    let(:call_attrs) { { id: case_id, method: method_name } }
-    let(:case_id) { c4s3.id }
+    let(:call_attrs) { { id: id } }
+    let(:id) { register.id }
+    let(:register) { create(:register) }
     let(:c4s3) { create(:case, type: type) }
+    let!(:link) { create(:case_register, case: c4s3, register: register) }
     let(:type) { 'test_case' }
-    let(:method_name) { 'a_method' }
     let(:dir) { "#{$root}/spec/fixtures/logic" }
     let(:logic) { CaseCore::Logic::Loader.logic(type) }
     let(:status_records) { CaseCore::Models::ProcessingStatus }
     let(:status_record) { status_records.where(message_id: message_id).last }
     let(:status) { status_record&.status }
 
-    it 'should call the method' do
+    it 'should call `export_register` function' do
       expect(logic)
-        .to receive(method_name)
-        .with(instance_of(CaseCore::Models::Case))
+        .to receive(:export_register)
+        .with(instance_of(CaseCore::Models::Register))
       subject
     end
 
@@ -59,7 +61,7 @@ RSpec.describe CaseCore::API::STOMP::Controller do
       let(:body) { 'not a JSON-string' }
 
       it 'should not call the method' do
-        expect(logic).not_to receive(method_name)
+        expect(logic).not_to receive(:export_register)
         subject
       end
 
@@ -73,7 +75,7 @@ RSpec.describe CaseCore::API::STOMP::Controller do
       let(:call_attrs) { 'not of Hash' }
 
       it 'should not call the method' do
-        expect(logic).not_to receive(method_name)
+        expect(logic).not_to receive(:export_register)
         subject
       end
 
@@ -83,11 +85,11 @@ RSpec.describe CaseCore::API::STOMP::Controller do
       end
     end
 
-    context 'when body is a JSON-string of Hash of wrong structure' do
-      let(:call_attrs) { { wrong: :structure } }
+    context 'when `id` parameter is absent' do
+      let(:call_attrs) { {} }
 
       it 'should not call the method' do
-        expect(logic).not_to receive(method_name)
+        expect(logic).not_to receive(:export_register)
         subject
       end
 
@@ -97,11 +99,11 @@ RSpec.describe CaseCore::API::STOMP::Controller do
       end
     end
 
-    context 'when case record can\'t be found' do
-      let(:case_id) { 'won\'t be found' }
+    context 'when a parameter beside `id` is present' do
+      let(:call_attrs) { { id: id, a: :parameter } }
 
       it 'should not call the method' do
-        expect(logic).not_to receive(method_name)
+        expect(logic).not_to receive(:export_register)
         subject
       end
 
@@ -111,8 +113,13 @@ RSpec.describe CaseCore::API::STOMP::Controller do
       end
     end
 
-    context 'when logic can\'t be found' do
-      let(:type) { 'won\'t be found' }
+    context 'when register is empty' do
+      let!(:link) {}
+
+      it 'should not call the method' do
+        expect(logic).not_to receive(:export_register)
+        subject
+      end
 
       it 'should have `error` status' do
         subject
@@ -120,9 +127,24 @@ RSpec.describe CaseCore::API::STOMP::Controller do
       end
     end
 
-    context 'when method is absent' do
-      let(:call_attrs) { { id: case_id, method: absent_method_name } }
-      let(:absent_method_name) { 'absent' }
+    context 'when logic can\'t be found by first case' do
+      let(:c4s3) { create(:case, type: 'wrong') }
+
+      it 'should not call the method' do
+        expect(logic).not_to receive(:export_register)
+        subject
+      end
+
+      it 'should have `error` status' do
+        subject
+        expect(status).to be == 'error'
+      end
+    end
+
+    context 'when logic doesn\'t provide `export_register` method' do
+      before do
+        allow(logic).to receive(:export_register).and_raise(NoMethodError)
+      end
 
       it 'should have `error` status' do
         subject
@@ -131,7 +153,7 @@ RSpec.describe CaseCore::API::STOMP::Controller do
     end
 
     context 'when an error appears during call' do
-      before { allow(logic).to receive(method_name).and_raise('') }
+      before { allow(logic).to receive(:export_register).and_raise('') }
 
       it 'should have `error` status' do
         subject
