4da6f39966cb3824433a26f731070c68ee76edfd
Ильчуков Александр Сергеевич <a.s.ilchukov@cit.rkomi.ru>
2018-01-30 12:15:26 +0300

    #39 Изменена бизнес-логика создания заявки
    Изменена бизнес-логика создания заявки следующим образом:
    *   отсутствие модуля бизнес-логики для предоставленного типа заявки
        теперь приводит к созданию исключения и отмене транзакции создания
        записей заявки и ассоциированных с ней записей атрибутов и
        документов;
    *   отсутствие функции `on_case_creation` в модуле бизнес-логики для
        предоставленного типа заявки теперь приводит к созданию исключения и
        отмене транзакции создания записей заявки и ассоциированных с ней
        записей атрибутов и документов;
    *   исключения класса `ArgumentError` или классов, наследующих от класса
        `ArgumentError`, которые созданы при вызове функции
        `on_case_creation` модуля бизнес-логики для предоставленного типа
        заявки теперь не блокируются.

--- spec/case_core/api/stomp/cases/create_spec.rb
+++ spec/case_core/api/stomp/cases/create_spec.rb
@@ -18,29 +18,25 @@ RSpec.describe CaseCore::API::STOMP::Controller do
       allow(Stomp::Client).to receive(:new).and_return(client)
 
       allow(CaseCore::API::STOMP::Controller.instance).to receive(:sleep)
+
+      CaseCore::Logic::Loader.settings.dir = dir
     end
 
     subject(:run!) { described_class.run! }
 
+    let(:dir) { "#{$root}/spec/fixtures/logic" }
     let(:message) { create(:stomp_message, headers: headers, body: body) }
     let(:headers) { create_incoming_headers(message_id, entities, action) }
     let(:message_id) { 'id' }
     let(:entities) { 'cases' }
     let(:action) { 'create' }
     let(:body) { case_attrs.to_json }
-    let(:case_attrs) { { type: :type } }
+    let(:case_attrs) { { type: type, **attrs, **documents } }
+    let(:attrs) { {} }
+    let(:documents) { {} }
     let(:status_records) { CaseCore::Models::ProcessingStatus }
     let(:status_record) { status_records.where(message_id: message_id).last }
     let(:status) { status_record&.status }
-
-    it 'should create a record of `CaseCore::Models::Case` model' do
-      expect { subject }.to change { CaseCore::Models::Case.count }.by(1)
-    end
-
-    it 'should have `ok` status' do
-      subject
-      expect(status).to be == 'ok'
-    end
 
     context 'when body is not a JSON-string' do
       let(:body) { 'not a JSON-string' }
@@ -81,64 +77,234 @@ RSpec.describe CaseCore::API::STOMP::Controller do
       end
     end
 
-    context 'when `id` attribute is not specified' do
-      it 'should create value of the attribute' do
-        expect { subject }.to change { CaseCore::Models::Case.count }.by(1)
+    context 'when there is no module of business logic for the case' do
+      let(:type) { 'no module for the case' }
+      let(:attrs) { { attr1: :value1, attr2: :value2 } }
+      let(:documents) { { documents: [{ id: :id }, { id: :id2 }] } }
+
+      it 'should have `error` status' do
+        subject
+        expect(status).to be == 'error'
       end
 
-      it 'should have `ok` status' do
-        subject
-        expect(status).to be == 'ok'
+      it 'shouldn\'t create case records' do
+        expect { subject }.not_to change { CaseCore::Models::Case.count }
+      end
+
+      it 'shouldn\'t create records of case attributes' do
+        expect { subject }
+          .not_to change { CaseCore::Models::CaseAttribute.count }
+      end
+
+      it 'shouldn\'t create records of documents' do
+        expect { subject }.not_to change { CaseCore::Models::Document.count }
       end
     end
 
-    context 'when `id` attribute is specified' do
-      let(:case_attrs) { { id: id, type: :type } }
-      let(:id) { 'id' }
+    context 'when there is a module of business logic for the case' do
+      let(:type) { 'test_case' }
 
-      context 'when a record with the value exists' do
-        let!(:case) { create(:case, id: id, type: :type) }
-
-        it 'should not create any record of `CaseCore::Models::Case` model' do
-          expect { subject }.not_to change { CaseCore::Models::Case.count }
-        end
+      context 'when the module doesn\'t provide `on_case_creation` function' do
+        let(:attrs) { { attr1: :value1, attr2: :value2 } }
+        let(:documents) { { documents: [{ id: :id }, { id: :id2 }] } }
 
         it 'should have `error` status' do
           subject
           expect(status).to be == 'error'
         end
-      end
 
-      context 'when a record with the value doesn\'t exist' do
-        let(:c4s3) { CaseCore::Models::Case.last }
-
-        it 'should use specified value' do
-          subject
-          expect(c4s3.id).to be == id
+        it 'shouldn\'t create case records' do
+          expect { subject }.not_to change { CaseCore::Models::Case.count }
         end
 
-        it 'should have `ok` status' do
-          subject
-          expect(status).to be == 'ok'
+        it 'shouldn\'t create records of case attributes' do
+          expect { subject }
+            .not_to change { CaseCore::Models::CaseAttribute.count }
+        end
+
+        it 'shouldn\'t create records of documents' do
+          expect { subject }.not_to change { CaseCore::Models::Document.count }
         end
       end
-    end
 
-    context 'when there are attributes besides `id` and `type`' do
-      let(:case_attrs) { { type: :type, attr1: :value1, attr2: :value2 } }
+      context 'when the module provides `on_case_creation` function' do
+        before { allow(logic).to receive(:on_case_creation) }
 
-      it 'should create records of `CaseCore::Models::CaseAttribute` model' do
-        expect { subject }
-          .to change { CaseCore::Models::CaseAttribute.count }
-          .by(2)
-      end
-    end
+        let(:logic) { CaseCore::Logic::Loader.logic(type) }
 
-    context 'when there are documents linked to the case' do
-      let(:case_attrs) { { type: :type, documents: [id: :id] } }
+        it 'should call the function' do
+          expect(logic)
+            .to receive(:on_case_creation)
+            .with(CaseCore::Models::Case)
+          subject
+        end
 
-      it 'should create records of `CaseCore::Models::Document` model' do
-        expect { subject }.to change { CaseCore::Models::Document.count }.by(1)
+        context 'when the function raises `ArgumentError`' do
+          before do
+            allow(logic).to receive(:on_case_creation).and_raise(error)
+          end
+
+          let(:error) { ArgumentError.new }
+          let(:attrs) { { attr1: :value1, attr2: :value2 } }
+          let(:documents) { { documents: [{ id: :id }, { id: :id2 }] } }
+
+          it 'should have `error` status' do
+            subject
+            expect(status).to be == 'error'
+          end
+
+          it 'shouldn\'t create case records' do
+            expect { subject }.not_to change { CaseCore::Models::Case.count }
+          end
+
+          it 'shouldn\'t create records of case attributes' do
+            expect { subject }
+              .not_to change { CaseCore::Models::CaseAttribute.count }
+          end
+
+          it 'shouldn\'t create records of documents' do
+            expect { subject }
+              .not_to change { CaseCore::Models::Document.count }
+          end
+        end
+
+        context 'when the function raises other errors' do
+          before do
+            allow(logic).to receive(:on_case_creation).and_raise(NameError)
+          end
+
+          it 'should have `ok` status' do
+            subject
+            expect(status).to be == 'ok'
+          end
+
+          it 'should create a record of `CaseCore::Models::Case` model' do
+            expect { subject }.to change { CaseCore::Models::Case.count }.by(1)
+          end
+
+          context 'when `id` attribute is not specified' do
+            it 'should create value of the attribute' do
+              expect { subject }
+                .to change { CaseCore::Models::Case.count }
+                .by(1)
+            end
+          end
+
+          context 'when `id` attribute is specified' do
+            let(:case_attrs) { { id: id, type: type } }
+            let(:id) { 'id' }
+
+            context 'when a record with the value exists' do
+              let!(:case) { create(:case, id: :id, type: type) }
+
+              it 'should have `error` status' do
+                subject
+                expect(status).to be == 'error'
+              end
+            end
+
+            context 'when a record with the value doesn\'t exist' do
+              let(:c4s3) { CaseCore::Models::Case.last }
+
+              it 'should have `ok` status' do
+                subject
+                expect(status).to be == 'ok'
+              end
+
+              it 'should use specified value' do
+                subject
+                expect(c4s3.id).to be == id
+              end
+            end
+          end
+
+          context 'when there are attributes besides `id` and `type`' do
+            let(:attrs) { { attr1: :value1, attr2: :value2 } }
+
+            it 'should create records of case attributes' do
+              expect { subject }
+                .to change { CaseCore::Models::CaseAttribute.count }
+                .by(2)
+            end
+          end
+
+          context 'when there are documents linked to the case' do
+            let(:documents) { { documents: [{ id: :id }, { id: :id2 }] } }
+
+            it 'should create records of documents' do
+              expect { subject }
+                .to change { CaseCore::Models::Document.count }
+                .by(2)
+            end
+          end
+        end
+
+        context 'when the function doesn\'t raise an error' do
+          it 'should have `ok` status' do
+            subject
+            expect(status).to be == 'ok'
+          end
+
+          it 'should create a record of `CaseCore::Models::Case` model' do
+            expect { subject }.to change { CaseCore::Models::Case.count }.by(1)
+          end
+
+          context 'when `id` attribute is not specified' do
+            it 'should create value of the attribute' do
+              expect { subject }
+                .to change { CaseCore::Models::Case.count }
+                .by(1)
+            end
+          end
+
+          context 'when `id` attribute is specified' do
+            let(:case_attrs) { { id: id, type: type } }
+            let(:id) { 'id' }
+
+            context 'when a record with the value exists' do
+              let!(:case) { create(:case, id: :id, type: type) }
+
+              it 'should have `error` status' do
+                subject
+                expect(status).to be == 'error'
+              end
+            end
+
+            context 'when a record with the value doesn\'t exist' do
+              let(:c4s3) { CaseCore::Models::Case.last }
+
+              it 'should have `ok` status' do
+                subject
+                expect(status).to be == 'ok'
+              end
+
+              it 'should use specified value' do
+                subject
+                expect(c4s3.id).to be == id
+              end
+            end
+          end
+
+          context 'when there are attributes besides `id` and `type`' do
+            let(:attrs) { { attr1: :value1, attr2: :value2 } }
+
+            it 'should create records of case attributes' do
+              expect { subject }
+                .to change { CaseCore::Models::CaseAttribute.count }
+                .by(2)
+            end
+          end
+
+          context 'when there are documents linked to the case' do
+            let(:documents) { { documents: [{ id: :id }, { id: :id2 }] } }
+
+            it 'should create records of documents' do
+              expect { subject }
+                .to change { CaseCore::Models::Document.count }
+                .by(2)
+            end
+          end
+        end
       end
     end
   end
