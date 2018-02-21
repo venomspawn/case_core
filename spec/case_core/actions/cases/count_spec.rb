30cce9aed92a1c36dbcf484250333176ac7d2b48
Ильчуков Александр Сергеевич <a.s.ilchukov@cit.rkomi.ru>
2018-02-21 17:18:32 +0300

    #47 Добавлен класс действия подсчёта количества заявок
    * Добавлен класс `CaseCore::Actions::Cases::Count` действия подсчёта
      количества заявок
    
    * Добавлены тесты класса `CaseCore::Actions::Cases::Count`
    
    * Добавлена функция `count` модуля `CaseCore::Actions::Cases` подсчёта
      количества заявок
    
    * Добавлены тесты функции `count` модуля `CaseCore::Actions::Cases`

---
+++ spec/case_core/actions/cases/count_spec.rb
@@ -0,0 +1,201 @@
+# encoding: utf-8
+
+# @author Александр Ильчуков <a.s.ilchukov@cit.rkomi.ru>
+#
+# Файл тестирования класса действия получения информации о количестве заявок
+#
+
+RSpec.describe CaseCore::Actions::Cases::Count do
+  describe 'the class' do
+    subject { described_class }
+
+    it { is_expected.to respond_to(:new) }
+  end
+
+  describe '.new' do
+    subject(:result) { described_class.new(params) }
+
+    let(:params) { {} }
+
+    describe 'result' do
+      subject { result }
+
+      it { is_expected.to be_a(described_class) }
+    end
+
+    context 'when argument is not of Hash type' do
+      let(:params) { 'not of Hash type' }
+
+      it 'should raise JSON::Schema::ValidationError' do
+        expect { subject }.to raise_error(JSON::Schema::ValidationError)
+      end
+    end
+
+    context 'when argument is of Hash type but of wrong structure' do
+      let(:params) { { filter: :wrong } }
+
+      it 'should raise JSON::Schema::ValidationError' do
+        expect { subject }.to raise_error(JSON::Schema::ValidationError)
+      end
+    end
+  end
+
+  describe 'instance' do
+    subject { described_class.new(params) }
+
+    let(:params) { {} }
+
+    it { is_expected.to respond_to(:count) }
+  end
+
+  describe '#count' do
+    include described_class::SpecHelper
+
+    subject(:result) { instance.count }
+
+    let(:instance) { described_class.new(params) }
+    let(:params) { {} }
+    let!(:cases) { create_cases }
+
+    describe 'result' do
+      subject { result }
+
+      it { is_expected.to match_json_schema(described_class::RESULT_SCHEMA) }
+
+      describe 'value of `count` attribute' do
+        subject(:count) { result[:count] }
+
+        it 'should be count of filtered cases' do
+          expect(subject).to be == 5
+        end
+
+        context 'when `filter` parameter is specified' do
+          let(:params) { { filter: filter } }
+
+          context 'when the parameter value is a map' do
+            context 'when a value of the map is a map' do
+              context 'when there is `exclude` key' do
+                let(:filter) { { rguid: { exclude: '101' } } }
+
+                it 'should be count of cases filtered by `exclude` value' do
+                  expect(subject).to be == 4
+                end
+              end
+
+              context 'when there is `like` key' do
+                let(:filter) { { rguid: { like: '%000%' } } }
+
+                it 'should be count of cases filtered by likely value' do
+                  expect(subject).to be == 3
+                end
+              end
+
+              context 'when there is `min` key' do
+                let(:filter) { { state: { min: 'error' } } }
+
+                it 'should be count of cases with values no less than value' do
+                  expect(subject).to be == 4
+                end
+              end
+
+              context 'when there is `max` key' do
+                let(:filter) { { state: { max: 'error' } } }
+
+                it 'should be count of cases with values no more than value' do
+                  expect(subject).to be == 2
+                end
+              end
+
+              context 'when there is more than one supported key specified' do
+                let(:filter) { { state: { min: 'error', exclude: 'ok' } } }
+
+                it 'should be count of cases filtered by filters together' do
+                  expect(subject).to be == 2
+                end
+              end
+
+              context 'when there is a key but it\'s unsupported' do
+                let(:filter) { { state: { unsupported: :key } } }
+
+                it 'should raise JSON::Schema::ValidationError' do
+                  expect { subject }
+                    .to raise_error { JSON::Schema::ValidationError }
+                end
+              end
+
+              context 'when there is no any key' do
+                let(:filter) { { state: {} } }
+
+                it 'should raise JSON::Schema::ValidationError' do
+                  expect { subject }
+                    .to raise_error { JSON::Schema::ValidationError }
+                end
+              end
+            end
+
+            context 'when a value is a list' do
+              let(:filter) { { state: %w(ok error) } }
+
+              it 'should be count of cases with values from the list' do
+                expect(subject).to be == 3
+              end
+            end
+
+            context 'when a value is not a list nor a map' do
+              let(:filter) { { state: 'ok' } }
+
+              it 'should be count of cases with the value' do
+                expect(subject).to be == 2
+              end
+            end
+          end
+
+          context 'when the parameter value is a list' do
+            context 'when the list is empty' do
+              let(:filter) { [] }
+
+              it 'should be count of all cases' do
+                expect(subject).to be == 5
+              end
+            end
+
+            context 'when the list contains filters' do
+              let(:filter) { [{ state: 'ok' }, { op_id: '2abc' }] }
+
+              it 'should be count of cases selected by at least one filter' do
+                expect(subject).to be == 3
+              end
+            end
+          end
+        end
+
+        context 'when `limit` parameter is specified' do
+          let(:params) { { limit: limit } }
+          let(:limit) { 2 }
+
+          it 'should be count of cases no more than the limit' do
+            expect(subject).to be <= limit
+          end
+        end
+
+        context 'when `offset` parameter is specified' do
+          let(:params) { { offset: offset } }
+          let(:offset) { 2 }
+
+          it 'should be count of cases shifted by offset' do
+            expect(subject).to be == 3
+          end
+        end
+
+        context 'when all supported parameters are specified' do
+          let(:params) { { filter: filter, limit: 2, offset: 1 } }
+          let(:filter) { [{ state: 'ok' }, { rguid: { like: '%000%' } } ] }
+
+          it 'should be count of filtered cases' do
+            expect(subject).to be == 2
+          end
+        end
+      end
+    end
+  end
+end
---
+++ spec/helpers/case_core/actions/cases/count/spec_helper.rb
@@ -0,0 +1,72 @@
+# encoding: utf-8
+
+module CaseCore
+  module Actions
+    module Cases
+      class Count
+        # @author Александр Ильчуков <a.s.ilchukov@cit.rkomi.ru>
+        #
+        # Вспомогательный модуль, предназначенный для включения в тесты
+        # содержащего класса
+        #
+        module SpecHelper
+          # Ассоциативный массив, в котором моделям соответствуют списки
+          # импортируемых значений
+          #
+          DATA = [
+            {
+              id:         '1',
+              type:       'test_case',
+              created_at: Time.now,
+              op_id:      '1abc',
+              state:      'ok',
+              rguid:      '101'
+            },
+            {
+              id:         '2',
+              type:       'test_case',
+              created_at: Time.now,
+              op_id:      '2abc',
+              state:      'error',
+              rguid:      '1001'
+            },
+            {
+              id:         '3',
+              type:       'spec_case',
+              created_at: Time.now,
+              op_id:      '2bbc',
+              state:      'closed',
+              rguid:      '10001'
+            },
+            {
+              id:         '4',
+              type:       'spec_case',
+              created_at: Time.now,
+              op_id:      '2bbb',
+              state:      'issue',
+              rguid:      '100001'
+            },
+            {
+              id:         '5',
+              type:       'spec_case',
+              created_at: Time.now,
+              op_id:      '3abc',
+              state:      'ok',
+              rguid:      '1000001'
+            }
+          ]
+
+          # Создаёт записи заявок вместе с записями атрибутов, после чего
+          # возвращает созданные записи заявок
+          #
+          # @return [Array<CaseCore::Models::Case>]
+          #   список созданных записей заявок
+          #
+          def create_cases
+            FactoryGirl.create(:imported_cases, data: DATA)
+          end
+        end
+      end
+    end
+  end
+end
