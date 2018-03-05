8d26f16bb753389a5b4d69f99bb398128112bf7c
Ильчуков Александр Сергеевич <a.s.ilchukov@cit.rkomi.ru>
2018-03-05 12:24:24 +0300

    #60 Добавлена поддержка случайных названий очередей

--- config/initializers/03_stomp.rb
+++ config/initializers/03_stomp.rb
@@ -12,13 +12,23 @@ require "#{$lib}/api/stomp/controller.rb"
 connection_info =
   YAML.load(ERB.new(IO.read("#{$root}/config/stomp.yml")).result)
 
+# Значение переменных окружения, означающее, что необходимо передать строки,
+# созданные с помощью случайных значений
+random_queue = '<random>'
+
 # Настройка очереди управляющих сообщений
-incoming_queue = ENV['CC_STOMP_INCOMING_QUEUE'] || 'case_core.incoming.queue'
+incoming_queue = ENV['CC_STOMP_INCOMING_QUEUE'] || random_queue
+if incoming_queue == random_queue
+  incoming_queue = format('case_core.incoming-%03d.queue', rand(1..999))
+end
 
 # Настройка очередей ответных сообщений СМЭВ. Названия очередей могут быть
 # перечислены через запятую в соответствующей переменной окружения.
-response_queues = ENV['CC_STOMP_RESPONSE_QUEUE'] || 'case_core.response.queue'
-response_queues = response_queues.split(',')
+response_queues = ENV['CC_STOMP_RESPONSE_QUEUE'] || '<random>'
+response_queues = response_queues.split(',').map do |queue|
+  next queue unless queue == random_queue
+  format('case_core.response-%03d.queue', rand(1..999))
+end
 
 # Установка конфигурации STOMP-контроллера
 CaseCore::API::STOMP::Controller.configure do |settings|
